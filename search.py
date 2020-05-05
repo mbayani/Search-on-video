0f35f44ce978d4f5bf7eac872a4c8dee5277601e
Amit Agrawal <58669645+amit970@users.noreply.github.com>
2020-05-05 15:58:00 -0400

    Update search.py
    Added performORBKeypointsSearch
    Fixed performKeypointsSearch using os.scandir instead of glob

--- search.py
+++ search.py
@@ -38,14 +38,17 @@ class Search:
 		#numberOfImages = len([name for name in os.listdir(self.dataset_path) if os.path.isfile(os.path.join(self.dataset_path, name))])
 		matchImages = []
 
-		for imagePath in glob.glob(self.dataset_path + "/*.*"):			
+		for obj in os.scandir(self.dataset_path):
+			imageFileName = obj.name
+			print("imageFileName:%s"%imageFileName)
+			imagePath = os.path.join(self.dataset_path, imageFileName)
 			image = cv2.imread(imagePath)
 			keypoints, descriptors = self.image_features.imageKeypoints(image)
 			bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
 			matches = bf.match(descriptors,query_descriptors)
 			matches = sorted(matches, key = lambda x:x.distance)
 			img3 = cv2.drawMatches(image, keypoints, query, query_keypoints, matches[:50], query, flags=2)
-			imageFileName = imagePath[imagePath.rfind("/") + 1:]
+			#imageFileName = imagePath[imagePath.rfind("/") + 1:]
 			imageExt = imageFileName.split('.')[1]
 			imageName = imageFileName.split('.')[0]
 			imageId = imageName.split('frame')[1]
@@ -65,3 +68,45 @@ class Search:
 			output.append(self.result_path + "/" + imgFileName)
 			matchOutput.append(self.result_path + "/" + imgMatchedFileName)
 		return output, matchOutput
+
+	def performORBKeypointsSearch(self):
+		output = []
+		matchOutput = []
+		resultMatches = []
+		print("performORBKeypointsSearch:query_path:%s"%self.query_path)
+		query_image = cv2.imread(self.query_path)
+		query_keypoints, query_descriptors = self.image_features.orbKeypoints(query_image)
+		ImgFileNames = []
+		matchesMatchedFileNames = []		
+		matchImages = []
+		
+		for obj in os.scandir(self.dataset_path):
+			imageFileName = obj.name
+			print(imageFileName)
+			imagePath = os.path.join(self.dataset_path, imageFileName)
+			image = cv2.imread(imagePath)
+			keypoints, descriptors = self.image_features.orbKeypoints(image)
+			bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+			matches = bf.match(descriptors,query_descriptors)
+			matches = sorted(matches, key = lambda x:x.distance)
+			img3 = cv2.drawMatches(image, keypoints, query_image, query_keypoints, matches[:20], query_image, flags=2)
+			imageExt = imageFileName.split('.')[1]
+			imageName = imageFileName.split('.')[0]
+			imageId = imageName.split('frame')[1]
+			resultMatch = (int(imageId), len(matches))
+			resultMatches.append(resultMatch)
+			matchImages.insert(int(imageId),img3)
+
+		searcher = Searcher(self.index_path)
+		results = searcher.searchKeypoints(resultMatches, limit=10)
+		for (score, resultID) in results:
+			imgFileName = 'frame%s.jpg'%resultID
+			imgMatchedFileName = 'orb_frame%s_match.jpg'%resultID
+			result = cv2.imread(os.path.join(self.dataset_path, imgFileName))
+			cv2.imwrite(os.path.join(self.result_path, 'orb_' + imgFileName), result)
+			matchedImage = matchImages[resultID]
+			cv2.imwrite(os.path.join(self.result_path, imgMatchedFileName), matchedImage)
+			output.append(os.path.join(self.result_path, 'orb_' + imgFileName))
+			matchOutput.append(os.path.join(self.result_path,imgMatchedFileName))
+		return output, matchOutput
+	
