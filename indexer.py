e3d84e920f3d50a299ac42a444b69b5ed2783fd8
Amit Agrawal <amita3@illinois.edu>
2020-05-05 17:19:13 -0400

    removed use of glob, using os.scandir

--- indexer.py
+++ indexer.py
@@ -1,5 +1,5 @@
 from features import ImageFeatures
-import glob
+import os
 import cv2
 
 class Indexer:
@@ -9,11 +9,12 @@ class Indexer:
 		self.image_feature = ImageFeatures((8, 12, 3))
 
 	def indexImages(self):
-		results = open(self.index_file_name, "w")
-		for imagePath in glob.glob(self.image_dataset_path + "/*.*"):
-			imageName = imagePath[imagePath.rfind("/") + 1:]
-			image = cv2.imread(imagePath)
-			features = self.image_feature.getFeatures(image)
-			features = [str(f) for f in features]
-			results.write("%s,%s\n" % (imageName, ",".join(features)))
-		results.close()
+		with open(self.index_file_name, "w") as results:
+			for obj in os.scandir(self.image_dataset_path):
+				imageName = obj.name
+				print("imageFileName:%s"%imageName)
+				imagePath = os.path.join(self.image_dataset_path, imageName)
+				image = cv2.imread(imagePath)
+				features = self.image_feature.getFeatures(image)
+				features = [str(f) for f in features]
+				results.write("%s,%s\n" % (imageName, ",".join(features)))
